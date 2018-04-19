43f8fc91fd09e62323c38fd95fdbeaa0285a81d0
Unknown <renerogeraraujo@gmail.com>
2018-04-19 16:27:46 -0300

    aula 95

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 
 class App extends Component {
@@ -64,7 +65,7 @@ class App extends Component {
 
     return (
 
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <Cockpit 
           title={this.props.title}
           showPersons={this.state.showPersons}
@@ -72,8 +73,7 @@ class App extends Component {
           clicked={this.tooglePersonsHandler}
           />
         {persons}
-
-      </div>
+      </WithClass>
 
 
     );
